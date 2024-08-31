1b27ba4e135e09b595c8de6ca933f294335cae54
vzero24 <120511169+vzero24@users.noreply.github.com>
2024-09-01 00:47:38 +0300

    db.php

--- db.php
+++ db.php
@@ -1,5 +1,13 @@
 <?php
-$con = new mysqli('localhost', 'root', '', 'users_app');
-if (!$con) {
-  die(mysqli_error($con));
+$servername = "db";             // The hostname should be 'db'
+$username = "root";             // The username is 'root'
+$password = "root_password";    // Replace with the actual root password from your docker-compose.yml
+$dbname = "users_app";
+
+// Create connection
+$con = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($con->connect_error) {
+  die("Connection failed: " . $con->connect_error);
 }
